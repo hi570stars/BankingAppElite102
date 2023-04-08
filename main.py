70c09a8e26310cbc5cf30c6448660eb6666c9618
Sneha Shekhar <sshekhar0128@gmail.com>
2023-04-07 22:14:20 -0500

    Adding log in/ create account message

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 print("Welcome to BetterBanking\nA bank that gives back.")
+input("1. Log in\n2. Create an account\n3. Exit")
 input("What would you like to do with us today?\n1. Check Your Balance\n2. Withdraw money\n3. Deposit Money\n4. Transfer Money\n5. Edit Account\n6. Exit and Log Out")
